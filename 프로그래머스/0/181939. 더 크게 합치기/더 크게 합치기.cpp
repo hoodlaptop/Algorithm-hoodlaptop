59c936e919cb76942d0ee2819350df5043aa8130
Developing Giroro <165354489+hoodlaptop@users.noreply.github.com>
2026-07-03 10:04:26 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.02 ms, Memory: 5.07 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.cpp
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.cpp
@@ -9,14 +9,7 @@ int solution(int a, int b)
     string temp1 = to_string(a) + to_string(b);
     string temp2 = to_string(b) + to_string(a);
     
-    if (stoi(temp1) > stoi(temp2))
-    {
-        answer = stoi(temp1);
-    }
-    else
-    {
-        answer = stoi(temp2);
-    }
+    answer = stoi(temp1) > stoi(temp2) ? stoi(temp1) : stoi(temp2);
     
     return answer;
 }
