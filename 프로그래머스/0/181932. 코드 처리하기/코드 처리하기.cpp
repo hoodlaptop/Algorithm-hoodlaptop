35e1182d08998ecae428e1a39e897e2866014af1
Developing Giroro <165354489+hoodlaptop@users.noreply.github.com>
2026-07-08 09:50:24 +0900

    [level 0] Title: 코드 처리하기, Time: 1.67 ms, Memory: 4.94 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.cpp
@@ -0,0 +1,42 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string code)
+{
+    string answer = "";
+
+    bool mode = false;
+    
+    for (int i = 0; i < code.length(); i++)
+    {
+        if (code[i] == '1')
+        {
+            mode = !mode;
+            continue;
+        }
+
+        if (mode == false)
+        {
+            if (i % 2 == 0)
+            {
+                answer += code[i];
+            }
+        }
+        else
+        {
+            if (i % 2 == 1)
+            {
+                answer += code[i];
+            }
+        }
+    }
+    
+    if (answer.empty())
+    {
+        return "EMPTY";
+    }
+    
+    return answer;
+}
