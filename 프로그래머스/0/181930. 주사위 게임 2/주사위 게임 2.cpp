d07198793146f927bf1fbb02056dbd049afbc182
Developing Giroro <165354489+hoodlaptop@users.noreply.github.com>
2026-07-10 10:23:54 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.01 ms, Memory: 5.07 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.cpp
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.cpp
@@ -1,3 +1,4 @@
+#include <set>
 #include <string>
 #include <vector>
 
@@ -6,19 +7,14 @@ using namespace std;
 int solution(int a, int b, int c)
 {
     int answer = 0;
-    
-    if (a != b && b != c && c != a)
-    {
+
+    set<int> s{a, b, c};
+    if (s.size() == 3)
         answer = a + b + c;
-    }
-    else if (a == b && b == c)
-    {
-        answer = (a + b + c) * (a*a + b*b + c*c) * (a*a*a + b*b*b + c*c*c);
-    }
-    else
-    {
+    if (s.size() == 2)
         answer = (a + b + c) * (a*a + b*b + c*c);
-    }
-    
+    if (s.size() == 1)
+        answer = (a + b + c) * (a*a + b*b + c*c) * (a*a*a + b*b*b + c*c*c);
+
     return answer;
 }
